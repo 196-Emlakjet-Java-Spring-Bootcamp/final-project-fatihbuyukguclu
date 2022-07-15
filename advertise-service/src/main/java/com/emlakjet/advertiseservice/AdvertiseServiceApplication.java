e657e4e9e3a1fbb10feaabdd0682df057d18e135
Fatih <curaat79@gmail.com>
2022-07-15 20:06:41 +0300

    Add Advertise Service Structure

---
+++ advertise-service/src/main/java/com/emlakjet/advertiseservice/AdvertiseServiceApplication.java
@@ -0,0 +1,15 @@
+package com.emlakjet.advertiseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@RefreshScope
+public class AdvertiseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvertiseServiceApplication.class, args);
+    }
+
+}
---
+++ advertise-service/src/test/java/com/emlakjet/advertiseservice/AdvertiseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emlakjet.advertiseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertiseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
