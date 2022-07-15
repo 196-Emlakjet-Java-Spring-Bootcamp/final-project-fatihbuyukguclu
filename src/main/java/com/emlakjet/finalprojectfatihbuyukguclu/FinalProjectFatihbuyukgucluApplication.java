634cf1b0bd58da17b25f8e29ac8da5823475cb4c
Fatih <curaat79@gmail.com>
2022-07-15 15:41:54 +0300

    Initial Commit

---
+++ src/main/java/com/emlakjet/finalprojectfatihbuyukguclu/FinalProjectFatihbuyukgucluApplication.java
@@ -0,0 +1,13 @@
+package com.emlakjet.finalprojectfatihbuyukguclu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectFatihbuyukgucluApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectFatihbuyukgucluApplication.class, args);
+    }
+
+}
