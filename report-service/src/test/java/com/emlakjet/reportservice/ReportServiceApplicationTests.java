f57c408bb15217e071ac2ddf57f5b45d5c16cc53
Fatih <curaat79@gmail.com>
2022-07-17 23:36:00 +0300

    Add Report Service

---
+++ report-service/src/test/java/com/emlakjet/reportservice/ReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emlakjet.reportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
